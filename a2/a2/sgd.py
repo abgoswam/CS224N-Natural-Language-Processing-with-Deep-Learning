91bf880b2b0d38bc7ac96ef92c96294d38660fed
Abhishek Goswami <abgoswam@gmail.com>
2019-01-20 11:45:36 -0800

    a2 coding part

--- a2/a2/sgd.py
+++ a2/a2/sgd.py
@@ -87,6 +87,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
         loss = None
         ### YOUR CODE HERE
 
+        loss, dx = f(x)
+        x -= (step*dx)
+
         ### END YOUR CODE
 
         x = postprocessing(x)
